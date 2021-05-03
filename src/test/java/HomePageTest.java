c874fd88bbfcf9207eb0b56a38a80589187b3486
lirownz <lironagam@gmail.com>
2021-05-03 15:14:45 +0300

    fix tasks

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -10,27 +10,27 @@ public class HomePageTest extends BaseTest {
     private HomePage homePage;
 
     @BeforeClass
-    public void setup(){
+    public void setup() {
         homePage = new HomePage(driver);
     }
 
     @BeforeMethod
-    public void setupMethod(){
+    public void setupMethod() {
         homePage.goToURL();
     }
 
-    @Test (priority=1)
-    public void checkRedirectingURL(){
-        Assert.assertEquals(driver.getCurrentUrl() , "https://www.facebook.com");
+    @Test(priority = 1)
+    public void checkRedirectingURL() {
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com");
     }
 
-    @Test (priority=2)
+    @Test(priority = 2)
     public void checkingCreateAnAcc() {
-            driver.findElement(By.linkText("Create New Account")).isDisplayed();
+        driver.findElement(By.linkText("Create New Account")).isDisplayed();
 
     }
 
-    @Test (priority=3)
+    @Test(priority = 3)
     public void registertion() {
         //enter registration page
         driver.findElement(By.linkText("Create New Account")).click();
@@ -41,6 +41,10 @@ public class HomePageTest extends BaseTest {
         //fill email
         driver.findElement(By.name("reg_email__")).sendKeys("0509998888");
         //fill new password
+    }
+
+    @Test(priority = 4)
+    public void dateOfBirth() {
         driver.findElement(By.id("password_step_input")).sendKeys("Ewave");
         //fill birth month
         Select month = new Select(driver.findElement(By.id("month")));
@@ -51,14 +55,23 @@ public class HomePageTest extends BaseTest {
         //fill birth year
         Select year = new Select(driver.findElement(By.id("year")));
         year.selectByValue("1988");
-        //select gender
-        driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[7]/span/span[3]/input")).click();
-        //clicking sing up button
-        driver.findElement(By.name("websubmit")).click();
     }
 
-    @Test (priority=4)
-    public void closeBrowser() {
-        teardown();
+    @Test(priority = 5)
+    public void genderPick() {
+        //select gender
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[7]/span/span[3]/input")).click();
     }
-}
+
+    @Test(priority = 6)
+    public void signUp() {
+        //clicking signUp button
+        driver.findElement(By.name("websubmit")).click();
+        }
+
+    @Test(priority = 7)
+    public void closeBrowser () {
+        teardown();
+        }
+    }
+
